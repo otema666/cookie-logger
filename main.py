cf49fa91a9b4a7982a809cce71f8dedb5f3335c0
otema666 <otema000@gmail.com>
2023-08-24 14:36:35 +0200

    Ahora el script también funciona en Linux

--- main.py
+++ main.py
@@ -23,9 +23,19 @@ urls = {
 }
 
 def clear():
-    os.system('cls')
+    os.system('cls') if os.name == "nt" else os.system("clear")
 
 clear()
+
+
+def rm_FirefoxLog():
+	if os.name == "nt":
+		os.system("del geckodriver.log")
+	else:
+		os.system("rm geckodriver.log")	
+
+if "geckodriver.log" in os.listdir():
+		rm_FirefoxLog()
 
 error = False
 while True:
@@ -51,12 +61,22 @@ while True:
         print(f'{G}Introduce el valor de la cookie de sesión de Instagram: \n{S}sessionid: {R}', end="")
         session_id_value = input()
         print(S)
-        driver = webdriver.Chrome()
+        
+        if os.name == "nt":
+            options = webdriver.ChromeOptions()
+            options.add_argument("--start-maximized")
+            options.add_argument("--incognito")
+            driver = webdriver.Chrome(options=options)
+        else:
+            options = webdriver.FirefoxOptions()
+            options.add_argument('--private-window')
+            driver = webdriver.Firefox(options=options)
+        
         driver.get("https://www.instagram.com/")
         clear()
         script = f"document.cookie = 'sessionid={session_id_value};path=/';"
         driver.execute_script(script)
-        print(f'Script: {script}')
+        # print(f'Script: {script}')
         driver.refresh()
         break
     elif url == "2" or url == "github" or url == "git":
@@ -64,9 +84,17 @@ while True:
         print(f'{G}Introduce el valor de la cookie de sesión de Github: \n{S}user_session: {R}', end="")
         user_session_value = input()
         print(S)
-        driver = webdriver.Chrome()
+        if os.name == "nt":
+            options = webdriver.ChromeOptions()
+            options.add_argument("--start-maximized")
+            options.add_argument("--incognito")
+            driver = webdriver.Chrome(options=options)
+        else:
+            options = webdriver.FirefoxOptions()
+            options.add_argument('--private-window')
+            driver = webdriver.Firefox(options=options)
         driver.get("https://www.github.com/")
-        os.system('cls')
+        clear()
         driver.execute_script(f"document.cookie = 'user_session={user_session_value};path=/';")
         driver.refresh()
         break
@@ -75,9 +103,17 @@ while True:
         print(f'{G}Introduce el valor de la cookie de sesión de Twitter: \n{S}auth_token: {R}', end="")
         auth_token_value = input()
         print(S)
-        driver = webdriver.Chrome()
+        if os.name == "nt":
+            options = webdriver.ChromeOptions()
+            options.add_argument("--start-maximized")
+            options.add_argument("--incognito")
+            driver = webdriver.Chrome(options=options)
+        else:
+            options = webdriver.FirefoxOptions()
+            options.add_argument('--private-window')
+            driver = webdriver.Firefox(options=options)
         driver.get("https://www.twitter.com/")
-        os.system('cls')
+        clear()
         driver.execute_script(f"document.cookie = 'auth_token={auth_token_value};path=/';")
         driver.refresh()
         driver.get("https://www.twitter.com/home/")
@@ -87,9 +123,17 @@ while True:
         print(f'{G}Introduce el valor de la cookie de sesión de TikTok: \n{S}sessionid: {R}', end="")
         sessionid_value = input()
         print(S)
-        driver = webdriver.Chrome()
+        if os.name == "nt":
+            options = webdriver.ChromeOptions()
+            options.add_argument("--start-maximized")
+            options.add_argument("--incognito")
+            driver = webdriver.Chrome(options=options)
+        else:
+            options = webdriver.FirefoxOptions()
+            options.add_argument('--private-window')
+            driver = webdriver.Firefox(options=options)
         driver.get("https://www.tiktok.com/")
-        os.system('cls')
+        clear()
         driver.execute_script(f"document.cookie = 'sessionid={sessionid_value};path=/';")
         driver.refresh()
         break
@@ -114,21 +158,32 @@ while True:
         print(f'{G}Introduce el valor de la cookie de sesión de Youtube: \n{S}{cookie}: {R}', end="")
         session_value = input()
         print(S)
-        driver = webdriver.Chrome()
+        if os.name == "nt":
+            options = webdriver.ChromeOptions()
+            options.add_argument("--start-maximized")
+            options.add_argument("--incognito")
+            driver = webdriver.Chrome(options=options)
+        else:
+            options = webdriver.FirefoxOptions()
+            options.add_argument('--private-window')
+            driver = webdriver.Firefox(options=options)
         driver.get("https://www.youtube.com/")
-        os.system('cls')
+        clear()
         script = f"document.cookie = '{cookie}={session_value};path=/; secure';"
         driver.execute_script(script) 
         driver.refresh()
         break
-    elif url == 6:
-        # clear()
+    elif url == "6":
+        input(f"No disponible {Fore.RESET}(press ENTER)")
+        clear()
         pass
-    elif url == 7:
-        # clear()
+    elif url == "7":
+        input(f"No disponible {Fore.RESET}(press ENTER)")
+        clear()
         pass
-    elif url == 8:
-        # clear()
+    elif url == "8":
+        input(f"No disponible {Fore.RESET}(press ENTER)")
+        clear()
         pass
     else:
         error = True
@@ -140,6 +195,7 @@ while True:
 
 print(f'{G}Presiona {B}ENTER{G} para cerrar el navegador.{S}')
 input()
+rm_FirefoxLog()
 # Cierra la ventana del navegador
 driver.quit()
 exit(1)
